03173fbd535f72aabed3b29c389379018e822eef
shehabalqudiry <omran.shehab40@gmail.com>
2020-10-02 23:12:00 +0200

    Upload Project To Repo

---
+++ parts/head_tags.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <link href="https://fonts.googleapis.com/css2?family=Changa&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Amiri&family=Changa&display=swap" rel="stylesheet">
+
+    <!-- Title -->
+    <title>Vizew - Blog &amp; Magazine HTML Template</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="img/core-img/icon.ico">
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body style="background-color: black;">
+    <!-- Preloader -->
+    <div class="preloader d-flex align-items-center justify-content-center">
+        <div class="lds-ellipsis">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
---
+++ parts/navbar.php
@@ -0,0 +1,98 @@
+ <!-- ##### Header Area Start ##### -->
+ <header class="header-area">
+        <!-- Top Header Area -->
+        <div class="top-header-area">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-12 col-md-6">
+                        <div class="top-meta-data d-flex align-items-center justify-content-end">
+                             <!-- Login -->
+                             <a href="login.html" class="login-btn"><i class="fa fa-user" aria-hidden="true"></i></a>
+                            <!-- Top Social Info -->
+                            <div class="top-social-info">
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-youtube-play"></i></a>
+                            </div>
+                           
+                            <!-- Top Search Area -->
+                            <div class="top-search-area">
+                                <form action="index.html" method="post">
+                                    <button type="submit" class="btn"><i class="fa fa-search" aria-hidden="true"></i></button>
+                                    <input type="search" name="top-search" id="topSearch" style="padding-right: 20px;" dir="rtl" placeholder="بحث .....">
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <!-- Breaking News Widget -->
+                        <div class="breaking-news-area d-flex align-items-center">
+                            <div id="breakingNewsTicker" class="ticker">
+                                <ul>
+                                    <li><a href="single-post.html">هنا يتم كتابة الخبر الأول</a></li>
+                                    <li><a href="single-post.html">هنا يتم كتابة الخبر الثانى</a></li>
+                                    <li><a href="single-post.html">هنا يتم كتابة الخبر الثالث</a></li>
+                                </ul>
+                            </div>
+                            <div class="news-title">
+                                <p>عاجل </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Navbar Area -->
+        <div class="vizew-main-menu" id="sticker">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="vizewNav">
+
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+
+                        <div class="classy-menu">
+
+                            <!-- Close Button -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+
+                            <!-- Nav Start -->
+                            <div class="classynav">
+                                <ul>
+                                    <li class="active"><a href="index.html">الرئيسية</a></li>
+                                    <li><a href="archive-list.html">سياسة</a></li>
+                                    <li><a href="archive-list.html">اقتصاد</a></li>
+                                    <li><a href="archive-list.html">حوادث</a></li>
+                                    <li><a href="contact.html">تواصل معنا</a></li>
+                                    <li><a href="archive-list.html">مواضيع أخرى</a></li>
+                                    <li><a href="#" dir="rtl"> رياضة </a>
+                                        <ul class="dropdown">
+                                            <li><a href="index.html">الدورى المصرى</a></li>
+                                            <li><a href="archive-list.html">الدورى الإنجليزى</a></li>
+                                            <li><a href="archive-grid.html">الدورى الأسبانى</a></li><hr>
+                                            <li><a href="single-post.html">كرة سلة</a></li>
+                                            <li><a href="video-post.html">سباحة</a></li>
+                                            <li><a href="contact.html">ألعاب قتالية</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="archive-list.html">عاجل</a></li>
+                                </ul>
+                            </div>
+                            <!-- Nav End -->
+                        </div>
+                        
+                        <!-- Nav brand -->
+                        <a href="index.html" class="nav-brand"><img src="img/core-img/logo.png" alt="logo"></a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ##### Header Area End ##### -->
